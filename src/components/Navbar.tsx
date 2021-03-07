050e128891c0a4c9b0da1cb235c6a3a202421ed0
Rory Lewis <lewisroryjames@gmail.com>
2021-03-07 13:30:26 +0100

    fixed navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,15 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Wrapper = styled.nav`
-    display: flex;
-    justify-content: center;
-    padding: 20px 10px;
+    width: 100%;
     @media (min-width: 800px) {
-        justify-content: flex-start;
-        padding: 20px 30px;
+        position: fixed;
         background: #000000;
         -webkit-box-shadow: 0px 5px 15px 1px rgba(0, 0, 0, 0.31);
         box-shadow: 0px 5px 15px 1px rgba(0, 0, 0, 0.31);
+    }
+`;
+const Inner = styled.div`
+    margin: 0 auto;
+    justify-content: center;
+    padding: 20px 10px;
+    display: flex;
+    @media (min-width: 800px) {
+        justify-content: flex-start;
+        padding: 20px 30px;
+        max-width: 900px;
+        width: 100%;
     }
 `;
 
@@ -22,7 +31,9 @@ const Logo = styled.h2`
 export default () => {
     return (
         <Wrapper>
-            <Logo>Crypto Check</Logo>
+            <Inner>
+                <Logo>Crypto Check</Logo>
+            </Inner>
         </Wrapper>
     );
 };
